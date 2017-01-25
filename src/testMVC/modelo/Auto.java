76852df1e677db36900743add231546c89822cd6
florandres <andreflo@ar.ibm.com>
2017-01-25 15:08:23 -0300

    Clase auto

--- src/testMVC/modelo/Auto.java
+++ src/testMVC/modelo/Auto.java
@@ -1,5 +1,65 @@
 package testMVC.modelo;
 
 public class Auto {
-
+	int patente;
+	String marca;
+	String modelo;
+	int año;
+	public Auto(int patente, String marca, String modelo, int año) {
+		this.patente = patente;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.año = año;
+	}
+	public int getPatente() {
+		return patente;
+	}
+	public void setPatente(int patente) {
+		this.patente = patente;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getAño() {
+		return año;
+	}
+	public void setAño(int año) {
+		this.año = año;
+	}
+	
+	
+	
 }
+
+
+
+
+
+
+
+/*    __**_**
+_**___**
+_**___**_________****
+_**___**_______**___****
+_**__**_______*___**___**
+__**__*______*__**__***__**
+___**__*____*__**_____**__*
+____**_**__**_**________**
+____**___**__**
+___*___________*
+__*_____________*
+_*____0_____0____*
+_*_______@_______*
+_*_______________*
+___*_____U_____*
+_____**_____** 
+*/
